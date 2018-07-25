fd7e0e285f4ae9914a52593a71b5e053cd64010d
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2018-07-25 13:52:07 +0200

    initial commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+docker run -t -i \
+-v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) \
+-v share:/home/reidar/share \
+-e USER=reidar \
+-e PATH=$PATH:/home/reidar/.local/bin \
+-e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
+-u reidar \
+-w /home/reidar \
+reidarcederqvist/iopsys /bin/bash
